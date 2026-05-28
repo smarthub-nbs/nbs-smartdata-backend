b97b58414ec4d8fa5d9f06c538babb60755b31db
lodyne <lodgmtui@gmail.com>
2026-05-28 20:06:08 +0300

    Add Django settings module and initialize app directory

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # Local apps
+    "djapps.user_management.apps.UserManagementConfig",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = "user_management.User"
