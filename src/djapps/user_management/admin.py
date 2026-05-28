4ed1dbc0c8faed5fa92f2eac7456b8ee3fe77e6b
lodyne <lodgmtui@gmail.com>
2026-05-28 20:06:16 +0300

    Refactor admin registration for user management models and update app name in configuration

--- src/djapps/user_management/admin.py
+++ src/djapps/user_management/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from .models import Permission, Role, RolePermission, User, UserRole
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "is_active")
+    search_fields = ("name", "code", "description")
+    list_filter = ("is_active",)
+
+
+class RolePermissionInline(admin.TabularInline):
+    model = RolePermission
+    extra = 0
+    autocomplete_fields = ("permission",)
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "is_active")
+    search_fields = ("name", "code", "description")
+    list_filter = ("is_active",)
+    inlines = (RolePermissionInline,)
+
+
+class UserRoleInline(admin.TabularInline):
+    model = UserRole
+    extra = 0
+    autocomplete_fields = ("role",)
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    ordering = ("email",)
+    list_display = ("email", "first_name", "last_name", "is_verified", "is_staff", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("is_verified", "is_staff", "is_superuser", "is_active")
+    readonly_fields = ("created_at", "updated_at", "last_login", "last_login_at")
+    filter_horizontal = ("groups", "user_permissions")
+    inlines = (UserRoleInline,)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Roles", {"fields": ("is_verified",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "last_login_at", "created_at", "updated_at")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "is_verified",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
