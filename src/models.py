81d113d5f8a9c667b48e8c1b72bd4ac5cbb5a02d
lodyne <lodgmtui@gmail.com>
2026-06-19 12:22:16 +0300

    Add UUID field to BaseModel for unique identification

--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils import timezone
 
@@ -33,6 +35,7 @@ class AllObjectsManager(models.Manager.from_queryset(SoftDeleteQuerySet)):
 
 
 class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
